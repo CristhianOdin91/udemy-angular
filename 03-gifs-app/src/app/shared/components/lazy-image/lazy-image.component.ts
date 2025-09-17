6cc351ccb23458964375769d90f564adde3dab19
Cristhian Odin Rueda Jiménez <cruedaji@montepiedad.com.mx>
2025-09-17 10:54:28 -0600

    Lazy Image - Parte 2

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
   standalone: false,
 })
-export class LazyImageComponent { }
+export class LazyImageComponent {
+  @Input()
+  url: string = '';
+
+  @Input()
+  alt: string = '';
+
+  private _hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) {
+      throw new Error('Url property is required');
+    }
+  }
+
+  get hasLoaded() {
+    return this._hasLoaded;
+  }
+  
+  onLoad() {
+    this._hasLoaded = true;
+  }
+}
